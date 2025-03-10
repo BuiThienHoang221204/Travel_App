43620cad98c2c0e42c200fcb5414018e8c4f67c6
TrinhHao42 <nguoila10112004@gmail.com>
2025-03-10 13:00:18 +0700

    pushcode

---
+++ backend/config/session/session.config.js
@@ -0,0 +1,8 @@
+const session = require('express-session');
+
+module.exports = session({
+    secret: process.env.SESSION_SECRET || 'your-secret-key',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }
+});
---
+++ fontend/src/config/routes.jsx
@@ -0,0 +1,7 @@
+const routes = {
+    home : '/',
+    about : '/about',
+    contact : '/contact',
+    login : '/login',
+}
+export default routes
