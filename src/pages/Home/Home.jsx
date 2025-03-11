4aefa44d3ebc046367fbb2b050a12c64efe21d6c
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-03-11 18:46:55 +0700

    hoang

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+// import React, { useState, useEffect } from 'react'
 
 import content1 from '../../assets/content-img1.jpg';
 import content2 from '../../assets/content-img2.jpg';
